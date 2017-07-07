81bcb70eccfff4db169cde9451c4801ddb21ca40
U-BEIJING\aaronliu <aaronliu@aaronliu.Beijing.Objectiva.local>
2017-07-07 19:09:06 +0800

    init

---
+++ app/Greeter.js
@@ -0,0 +1,6 @@
+//
+module.exports = function () {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greeting!";
+    return greet;
+}
---
+++ app/main.js
@@ -0,0 +1,3 @@
+//
+var greeter = require('./Greeter.js');
+document.getElementById('root').appendChild(greeter());
